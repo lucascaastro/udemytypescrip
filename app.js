476a32fe6acc849a9b6d443c9ed5c101b1c58213
lucascaastro <lucasgrrock@gmail.com>
2018-04-02 18:12:26 -0300

    aulas de interface example

--- app.js
+++
@@ -1,2 +0,0 @@
-var message = "Olá Mundo";
-console.log(message);
--- app.ts
+++ app.ts
@@ -1,22 +1,41 @@
-let message: string ="Olá Mundo"
-console.log(message); 
-
-let episode: number = 4
-console.log("This is episode " + 4)
-episode = episode + 1
-console.log("Next episode is " + episode)
-
-let favoriteDroid: string
-favoriteDroid = 'BB-8'
-console.log("My favorite droid is " + favoriteDroid)
-
-let isEnoughToBeatMF = function (parsecs: number) : boolean{
-    return parsecs < 12
+class Spacecraft {
+//examplo simplificado de construção de uma classe 
+  constructor (public propulsor: string ){}
+  
+  jumpIntoHyperspace(){
+      console.log(`Entering hyperspace with ${this.propulsor}`)
+      
+  }
 }
 
-let distance = 14
+let ship = new Spacecraft('hyperdrive')
+ship.jumpIntoHyperspace()
 
-console.log(` Is ${distance} parsecs enough to beat Millenium Falcon? `   )
+class MillenniumFalcon extends Spacecraft implements Containership{
+    
+    cargoContainers: number
 
+    constructor(){
+       super('hyperdrive')
+       this.cargoContainers = 4
+    }
 
- 
+    jumpIntoHyperspace(){
+        if (Math.random() >= 0.5){
+            super.jumpIntoHyperspace()
+        }else {console.log('Failed to jump into hyperspace') }
+        }
+    }
+
+    let falcon = new MillenniumFalcon()
+    falcon.jumpIntoHyperspace()
+
+    interface Containership{
+
+        cargoContainers: number
+    }
+
+    let goodForTheJob = (ship: Containership ) => ship.cargoContainers > 2
+
+    console.log(`Is falcon good for the job? ${goodForTheJob ( falcon )? 'yes' :'no'}`)
+    
