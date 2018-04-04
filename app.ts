f54f04040e03672dac7061c70b6fe9f784e94c09
lucascaastro <lucasgrrock@gmail.com>
2018-04-04 12:41:14 -0300

    exemplos de typescript terminados

--- app.ts
+++ app.ts
@@ -1,6 +1,10 @@
 import {Spacecraft, Containership} from './base-ships'
 import {MillenniumFalcon} from './starfighters'
 
+import * as _ from 'lodash'
+console.log(_.pad("Typescript Examples", 40,"="))
+
+
 let ship = new Spacecraft('hyperdrive')
 ship.jumpIntoHyperspace()
 
@@ -8,4 +12,4 @@ let falcon = new MillenniumFalcon()
 falcon.jumpIntoHyperspace()
     
 let goodForTheJob = (ship: Containership ) => ship.cargoContainers > 2
-console.log(`Is falcon good for the job? ${goodForTheJob ( falcon )? 'yes' :'no'}`)
+console.log(`Is falcon good for the job? ${goodForTheJob ( falcon )? 'yes' : 'no'}`)
